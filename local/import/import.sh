ffcc9324ed7d5898d0938561d6c862830579523a
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-01-16 13:27:34 +0100

    fixed a bug in import script

--- local/import/import.sh
+++ local/import/import.sh
@@ -6,10 +6,18 @@
 #   Path to the vufind installation
 #
 
+##################################################
+# Set VUFIND_HOME
+##################################################
+if [ -z "$VUFIND_HOME" ]
+then
+  VUFIND_HOME="/usr/local/vufind"
+fi
+
 for filename in $1*.gz 
 do 
 	echo "converting ${filename}"
 	ruby $VUFIND_HOME/local/import/aleph2marc.rb ${filename}
 	echo "importing ${filename}"
-	bash $VUFIND_HOME/import-marc.sh $VUFIND_HOME/local/import/mrc/$(basename "$filename" .txt).mrc
+	bash $VUFIND_HOME/import-marc.sh $VUFIND_HOME/local/import/mrc/$(basename "$filename" .gz).mrc
 done
