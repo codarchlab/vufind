5343a6ed37ae209f0a8be96ca51e7ba00885948e
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-01-16 14:51:59 +0100

    added bash script for updating data via oai-pmh

---
+++ local/harvest/update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Bash script to start the import of MARC-XML data harvested via OAI-PMH
+#
+# VUFIND_HOME
+#   Path to the vufind installation
+#
+# usage:
+#	update.sh DATE
+#
+# arguments:
+#	DATE in YYYY-MM-DD format sets the from argument for OAI-PMH
+#
+
+##################################################
+# Set VUFIND_HOME
+##################################################
+if [ -z "$VUFIND_HOME" ]
+then
+  VUFIND_HOME="/usr/local/vufind"
+fi
+
+php $VUFIND_HOME/harvest/harvest_oai.php --from $1
+$VUFIND_HOME/harvest/batch-import-marc.sh DAI_ALL
+$VUFIND_HOME/harvest/batch-delete.sh DAI_ALL
