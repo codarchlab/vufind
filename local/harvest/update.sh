f01b79369720f65bae616b8e9b7de7a671a1adff
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-02-24 18:30:44 +0100

    added ruby scripts for marc conversion and status mails

--- local/harvest/update.sh
+++ local/harvest/update.sh
@@ -20,6 +20,9 @@ then
   VUFIND_HOME="/usr/local/vufind"
 fi
 
-php $VUFIND_HOME/harvest/harvest_oai.php DAI_ALL --from $1
-$VUFIND_HOME/harvest/batch-import-marc.sh DAI_ALL
+today=$(date +"%Y-%m-%d")
+
+#php $VUFIND_HOME/harvest/harvest_oai.php DAI_ALL --from $1
+ruby $VUFIND_HOME/local/import/collect-marc-xml.rb $VUFIND_HOME/local/harvest/DAI_ALL/
+$VUFIND_HOME/harvest/batch-import-marc.sh DAI_ALL > $VUFIND_HOME/local/harvest/DAI_ALL/log/import_$today.log
 $VUFIND_HOME/harvest/batch-delete.sh DAI_ALL
---
+++ local/import/collect-marc-xml.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+#
+# Ruby script for collecting several marc XML files in one marc binary file
+#
+
+require 'marc'
+require 'logger'
+require 'fileutils'
+
+start_time = Time.new
+
+MARC::ControlField.control_tags.add("992")
+puts "using XML library: " + MARC::XMLReader.best_available!
+
+dir = ARGV[0]
+Dir.mkdir dir + 'collected' unless File.exists? dir + 'collected'
+Dir.mkdir dir + 'errors' unless File.exists? dir + 'errors'
+Dir.mkdir dir + 'log' unless File.exists? dir + 'log'
+output_file_name = 'collect_' + Time.new.strftime("%Y-%m-%d_%H-%M-%S");
+output_file = dir + output_file_name + '.mrc'
+logger = Logger.new(dir + 'log/' + output_file_name + ".log")
+writer = MARC::Writer.new(output_file)
+
+count = 0
+
+Dir.glob(dir + '*.xml') do |xml_file|
+	puts "reading #{xml_file}"
+	begin
+		reader = MARC::XMLReader.new(xml_file)
+		reader.each do |r|
+			writer.write r
+			FileUtils.mv(xml_file, dir + 'collected/')
+			count += 1
+		end
+	rescue => e
+		FileUtils.mv(xml_file, dir + 'errors/')
+    	msg = "Error while processing #{xml_file}: #{e.message}"
+    	puts msg
+    	logger.error msg
+	end
+end
+
+writer.close()
+puts "finished collecting MARC XML files into #{output_file}"
+
+end_time = Time.new
+duration = end_time - start_time
+seconds = duration % 60
+minutes = (duration / 60) % 60
+hours = duration / (60 * 60)
+avg = count / duration
+puts "processed #{count} records in #{format("%dh %dm %ds", hours, minutes, seconds)} (avg: #{avg.round(3)} records per second)"
