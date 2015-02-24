6cde0348481b667f4656da84b0427c861996aea8
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-02-24 18:36:31 +0100

    include harvesting logs in statusmail

--- local/harvest/update.sh
+++ local/harvest/update.sh
@@ -22,7 +22,7 @@ fi
 
 today=$(date +"%Y-%m-%d")
 
-#php $VUFIND_HOME/harvest/harvest_oai.php DAI_ALL --from $1
+php $VUFIND_HOME/harvest/harvest_oai.php DAI_ALL --from $1 > $VUFIND_HOME/local/harvest/DAI_ALL/log/harvest_$today.log
 ruby $VUFIND_HOME/local/import/collect-marc-xml.rb $VUFIND_HOME/local/harvest/DAI_ALL/
 $VUFIND_HOME/harvest/batch-import-marc.sh DAI_ALL > $VUFIND_HOME/local/harvest/DAI_ALL/log/import_$today.log
 $VUFIND_HOME/harvest/batch-delete.sh DAI_ALL
