23a48dbbb2362a2dd22d8d60b42802ab404b8d77
Till Kinstler <kinstler@gbv.de>
2016-09-15 12:27:43 +0200

    Einige Features in Entwicklung herausgenommen, um fertige Änderungen in den zenon-branch (-> Produktivversion) übernehmen zu können:
    > 1) FAQ-Button in der unteren Menüleiste soll auskommentiert werden
    
    Ist auskommentiert in themes/archaeostrap/templates/footer.phtml
    
    > 2) es soll keine Anzeige der Format-Facette erfolgen
    
    Auskommentiert in local/config/vufind/facets.ini
    
    > 3) Vufind-Gazetteer
    > 3a) Knopf idai.gazetteer: bisher für ztopog und zeurop (das soll
    > bleiben). Die Erweiterung für 3.00.. soll auskommentiert werden
    
    in module/Zenon/src/Zenon/RecordDriver/SolrMarc.php auskommentiert.

--- module/Zenon/src/Zenon/RecordDriver/SolrMarc.php
+++ module/Zenon/src/Zenon/RecordDriver/SolrMarc.php
@@ -299,13 +299,14 @@ class SolrMarc extends VufindSolrMarc
                  // use 999 $m (= $thsEntry['searchterm'] as additional
                  // parameter in Gazetteer link, if 999 $1 == 3.00.01.01.*
                  // or 999 $1 == 3.00.01.02.* (in $thsEntry['notation'])
-                 if (strrpos($thsEntry['notation'], '3.00.01.01', -strlen($thsEntry['notation'])) !== false
+// 15.09.2016 Übernahme nach zenon-branch
+/*                 if (strrpos($thsEntry['notation'], '3.00.01.01', -strlen($thsEntry['notation'])) !== false
                     || strrpos($thsEntry['notation'], '3.00.01.02', -strlen($thsEntry['notation'])) !== false) {
                       $result[] = array(
                                 'label' => $thsEntry['label'],
                                 'uri' => "http://gazetteer.dainst.org/app/#!/search?q=" . $thsEntry['notation'] . ";" . $thsEntry['searchterm']
                         );
-                 }
+                 } */
                  // use 999 $r (= $thsEntry['searchterm2'] as additional
                  // parameter in Gazetteer link, if 999 $1 == xtop* ($thsEntry['notation'])
                  if (strrpos($thsEntry['notation'], 'xtop', -strlen($thsEntry['notation'])) !== false ) {
