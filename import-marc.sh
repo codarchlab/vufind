18a58b4c4538bc1eedc53135dfa2ec97145aae6d
Simon Hohl <simon.hohl@dainst.org>
2017-06-30 09:53:48 +0200

    Increased available RAM for indexing process.

--- import-marc.sh
+++ import-marc.sh
@@ -49,7 +49,7 @@ fi
 ##################################################
 if [ -z "$INDEX_OPTIONS" ]
 then
-  INDEX_OPTIONS='-Xms512m -Xmx512m -DentityExpansionLimit=0'
+  INDEX_OPTIONS='-Xms1G -Xmx1G -DentityExpansionLimit=0'
 fi
 
 
