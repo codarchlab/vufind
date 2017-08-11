e542b2f08b1fe4fe83125bb1b3b6a09f609744c6
Simon Hohl <simon.hohl@dainst.org>
2017-08-11 13:29:08 +0200

    Strings in 041b get split into seperate subfields b when preprocessing marcxml/marc while indexing.

--- local/import/preprocess-marc.rb
+++ local/import/preprocess-marc.rb
@@ -4,6 +4,38 @@ require 'marc'
 require 'pp'
 require 'logger'
 
+
+def split_language_keys(record)
+
+  if record['041']['a'] and record['041']['b']
+    main_language_key_length = record['041']['a'].length
+    if record['041']['b'].kind_of?(String) and
+        record['041']['b'].length % main_language_key_length == 0 and
+        record['041']['b'].length != main_language_key_length
+
+      split_b = record['041']['b'].scan(/.{#{main_language_key_length}}/)
+      updated_field = MARC::DataField.new('041',record['indiciator1'],record['indiciator2'])
+
+      # copy all subfields except 'b' from original
+      record['041'].each do |s|
+        if(s.code != 'b')
+          updated_field.append(s)
+        end
+      end
+
+      # build new subfields 'b' from split values
+      split_b.each do |value|
+        updated_field.append(MARC::Subfield.new('b',value))
+      end
+
+      record.fields.delete(record['041'])
+      record.append(updated_field)
+    end
+  end
+
+  record
+end
+
 name = File.basename(ARGV[0], File.extname(ARGV[0]))
 
 vufind_home = ENV['VUFIND_HOME'] || '/usr/local/vufind'
@@ -27,6 +59,7 @@ for record in reader
       else
         record.append(MARC::ControlField.new('003', 'ZENON'))
       end
+      record = split_language_keys(record)
       writer.write(record)
     else
       error_writer.write(record)
