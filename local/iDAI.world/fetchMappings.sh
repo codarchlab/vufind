7cde13a36d19127de3b8219c5bdd8075e5262a2d
Simon Hohl <simon.hohl@dainst.org>
2018-02-09 10:27:05 +0100

    Fix broken logging for publication link harvesting

--- local/iDAI.world/fetchMappings.sh
+++ local/iDAI.world/fetchMappings.sh
@@ -8,4 +8,5 @@ then
   VUFIND_HOME="/usr/local/vufind"
 fi
 
+exec &> $VUFIND_HOME/local/iDAI.world/log/publications_`date +\%Y-\%m-\%d`.log
 curl -H "Accept: application/json" "https://publications.dainst.org/journals/plugins/pubIds/zenon/api.php" > $VUFIND_HOME/local/iDAI.world/publications_mapping.json
