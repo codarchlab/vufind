6c441020a66d5b214b3df219d1c699c5d5f5b92f
Simon Hohl <simon.hohl@dainst.org>
2020-01-08 16:21:37 +0100

    Link to parent record in holdings for serials
    See Redmine ticket #11770.

--- module/Zenon/src/Zenon/RecordDriver/SolrMarc.php
+++ module/Zenon/src/Zenon/RecordDriver/SolrMarc.php
@@ -240,30 +240,61 @@ class SolrMarc extends VufindSolrMarc
      *
      * @return array
      */
+    public function getLinkToSerialParentRecord(){
+
+        $fields =  $this->getMarcRecord()->getFields('590');
+        $validKeys = array(
+            'arom', 'argk', 'aath', 'aist', 'amad', 'aschw', 'alubl', 'aeur', 'acai', 'adam', 'abays', 'absa', 
+            'brom', 'brgk', 'bath', 'bist', 'bmad', 'bschw', 'blubl', 'bcai', 'beur', 'bbays', 'zbath', 'zbbays', 
+            'zbbsa', 'zbcai', 'zbdam', 'zbeur', 'zbist', 'zblubl', 'zbmad', 'zbori', 'zbrgk', 'zbrom', 'zbschw',
+            'zbz'
+        );
+
+        $found = false;
+        foreach($fields as $currentField) {
+            $key = $currentField->getSubfield('a');
+            if(!$key){
+                continue;
+            }
+            foreach($validKeys as $currentKey) {
+                $len = strlen($currentKey);
+                if(substr($key->getData(), 0, $len) === $currentKey){
+                    $found = true;
+                    break;
+                }
+            }
+            if($found) break;
+        }
+        if($found) {
+            return $this->getHostItemInformation();
+        }
+    }
+
+    /**
+     * Get the host item information (MARC 21 field 773).
+     *
+     * @return array
+     */
     public function getHostItemInformation()
     {
-        $results = [];
-
-        $customFieldData = $this->getCustomFieldHostItemLinkData();
-        if($customFieldData) {
-            $results = $customFieldData;
-        }
+        $result = [];
 
         $fields =  $this->getMarcRecord()->getFields('773');
         foreach($fields as $currentField) {
             $recordControlNumber = $currentField->getSubfield('w');
             if($recordControlNumber) {
                 $data = $this->getHostItemLinkData($currentField);
-                if($data) array_push($results, $data);
+                if($data) 
+                    $result = $data;
             }
             else {
                 $textEntry = $this->getSubfieldArray($currentField, ['a', 'b', 't', 'g', 'n', 'x'], false);
-                if(sizeOf($textEntry) > 0) {
-                    array_push($results, array('id' => null, 'label' => join(', ', $textEntry)));
-                }
+                
+                if(sizeOf($textEntry) > 0)
+                    $result = array('id' => null, 'label' => join(', ', $textEntry));
             }
         }
-        return $results;
+        return $result;
     }
 
     private function getHostItemLinkData($currentField)
@@ -290,13 +321,6 @@ class SolrMarc extends VufindSolrMarc
         else {
             return null;
         }
-    }
-
-    private function getCustomFieldHostItemLinkData()
-    {
-    	$linkType = 'ANA';
-
-        return $this->createCustomFieldLinkArray($linkType);
     }
 
     /**
