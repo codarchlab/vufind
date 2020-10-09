d01f8fde7fa705c8d91bf3991763328ea203ff41
Simon Hohl <simon.hohl@dainst.org>
2020-10-09 13:34:10 +0200

    SD-709: Switch to OAI harvesting

--- local/harvest/update.sh
+++ local/harvest/update.sh
@@ -22,8 +22,10 @@ fi
 
 today=$(date +"%Y-%m-%d")
 
-# TODO: Update for Koha
-# php $VUFIND_HOME/harvest/harvest_oai.php dai-katalog --from $1 > $VUFIND_HOME/local/harvest/dai-katalog/log/harvest_$today.log
-# ruby $VUFIND_HOME/local/import/collect-marc-xml.rb $VUFIND_HOME/local/harvest/dai-katalog/
-# $VUFIND_HOME/harvest/batch-import-marc.sh dai-katalog > $VUFIND_HOME/local/harvest/dai-katalog/log/import_$today.log
-# $VUFIND_HOME/harvest/batch-delete.sh dai-katalog
+mkdir -p $VUFIND_HOME/local/harvest/dai-katalog/log
+
+php $VUFIND_HOME/harvest/harvest_oai.php dai-katalog --from $1 2>&1 | tee $VUFIND_HOME/local/harvest/dai-katalog/log/harvest_$today.log
+python3 $VUFIND_HOME/combine-marc.py $VUFIND_HOME/local/harvest/dai-katalog 2>&1 | tee $VUFIND_HOME/local/harvest/dai-katalog/log/combine_$today.log
+python3 "$VUFIND_HOME"/preprocess-marc.py $VUFIND_HOME/local/harvest/dai-katalog/preprocess $VUFIND_HOME/local/harvest/dai-katalog --url "http://zenon.dainst.org" 2>&1 | tee $VUFIND_HOME/local/harvest/dai-katalog/log/preprocess_$today.log # "http://$(hostname -i)"
+$VUFIND_HOME/harvest/batch-import-marc.sh dai-katalog 2>&1 | tee $VUFIND_HOME/local/harvest/dai-katalog/log/import_$today.log
+$VUFIND_HOME/harvest/batch-delete.sh dai-katalog 2>&1 | tee $VUFIND_HOME/local/harvest/dai-katalog/log/delete_$today.log
