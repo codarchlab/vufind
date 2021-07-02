d7201352404e5557b2a6c2fff3a27241e0e5da71
Simon Hohl <simon.hohl@dainst.org>
2021-07-02 13:11:43 +0200

    Also read subfield $i for linking fields

--- module/Zenon/src/Zenon/RecordDriver/SolrMarc.php
+++ module/Zenon/src/Zenon/RecordDriver/SolrMarc.php
@@ -308,7 +308,7 @@ class SolrMarc extends VufindSolrMarc
     }
     
     private function getLinkedEntryLabel($field) {
-        $combinedGeneralSubfields = $this->getSubfieldArray($field, ['a', 'b', 't', 'g', 'n', 'x', 't'], true, ', ');
+        $combinedGeneralSubfields = $this->getSubfieldArray($field, ['a', 'b', 't', 'g', 'n', 'x', 't', 'i'], true, ', ');
         $relationshipInformation = $field->getSubfield('i');
 
         $label = null;
@@ -408,8 +408,7 @@ class SolrMarc extends VufindSolrMarc
     public function getParallelEditions()
     {
         {
-            return $this->createLinkingEntries($this->getMarcRecord()->getFields('776')); # Helena: übernommen aus getOtherRelationships()
-            # das ist aber noch nicht ideal, da hier die Art der Aufnahme nicht angezeigt wird.
+            return $this->createLinkingEntries($this->getMarcRecord()->getFields('776'));
         }
     }
 
